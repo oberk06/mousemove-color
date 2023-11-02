3559fb63526ce274ac264a6eb3b141f7e7603d41
oberk06 <oguzhanberk174@gmail.com>
2023-11-03 02:42:58 +0300

    first-step

---
+++ js.js
@@ -0,0 +1,31 @@
+const container = document.getElementById("container");
+const adet = 2520;
+const renkler = [
+  "#FF5733",
+  "#3498DB",
+  "#F1C40F",
+  "#27AE60",
+  "#8E44AD",
+  "#E74C3C",
+  "#F39C12",
+  "#2E4053",
+  "#D35400",
+  "#1ABC9C ",
+];
+
+for (i = 0; i < adet; i++) {
+  const kareler = document.createElement("div");
+  kareler.classList = "kare";
+  container.appendChild(kareler);
+ 
+
+  kareler.addEventListener("mousemove", () => {
+    let rastgele = Math.floor(Math.random() * renkler.length);
+    kareler.style.backgroundColor = renkler[rastgele];
+  });
+  kareler.addEventListener("touchstart", (event) => {
+    let rastgele = Math.floor(Math.random() * renkler.length);
+    kareler.style.backgroundColor = renkler[rastgele];
+    event.preventDefault(); // Dokunma olayının varsayılan davranışını önler
+  });
+}
